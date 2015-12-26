8bf8deee78ac53d35788574a71beb083cfb5f41f
jinisha2193 <jinisha2193@gmail.com>
2015-12-26 13:33:05 +0530

    addition and subtraction

---
+++ ArithmeticOperations/src/com/digitek/mathoperations/MathOperations.java
@@ -0,0 +1,30 @@
+package com.digitek.mathoperations;
+
+public class MathOperations {
+	
+	
+	
+	public static void addNumber(){
+		int a = 15;
+		int b = 20;
+		int c = a+b;
+		System.out.println("the sum of two numbers:"+c);
+		
+		
+		
+	}
+    public static void subtraction(){
+    	int a=15;
+    	int b = 10;
+    	int c = a-b;
+    	System.out.println("the difference of two numbers:"+c);
+		
+		
+		
+	}
+    public static void main(String args[]){
+    	addNumber();
+    	subtraction();
+    }
+
+}
